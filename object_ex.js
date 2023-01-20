f7477ab604f1c1fc7866e7b83540c8cc928ee0d3
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-01-20 11:19:25 +0200

    valmis

--- object_ex.js
+++ object_ex.js
@@ -1,1 +1,15 @@
 //objekti esimerkki
+const person={
+  fname:"Teppo",
+  lname:"Testi",
+  setFname:function(fn){
+    this.fname=fn;
+  } ,
+  getFname:function(){
+    return this.fname;
+  } 
+}
+
+console.log(person.getFname());
+person.setFname("Matti");
+console.log(person.getFname());
